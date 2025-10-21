f5da280d46c88b88f990f5f3a427ba54bc0e25a4
kerollos <kyrlsatf086@gmail.com>
2025-10-21 14:57:14 +0200

    add

--- Genetic_Algorithm_PH1/src/main/java/SoftComputing/interfaces/InfeasibilityHandler.java
+++ Genetic_Algorithm_PH1/src/main/java/SoftComputing/interfaces/InfeasibilityHandler.java
@@ -1,1 +1,8 @@
+package SoftComputing.interfaces;
 
+import SoftComputing.Chromosomes.Chromosome;
+
+public interface InfeasibilityHandler {
+    boolean isFeasible(Chromosome individual);
+    Chromosome repair(Chromosome individual);
+}
