08c87f8b4198e10098c97f1753a9d69a25d90b7d
kerollos <kyrlsatf086@gmail.com>
2025-10-22 13:51:37 +0200

    add

--- Genetic_Algorithm_PH1/src/main/java/SoftComputing/Core/InfeasibilityHandlerImpl.java
+++ Genetic_Algorithm_PH1/src/main/java/SoftComputing/Core/InfeasibilityHandlerImpl.java
@@ -1,40 +1,52 @@
 package SoftComputing.Core;
+
 import SoftComputing.Chromosomes.Chromosome;
 import SoftComputing.interfaces.InfeasibilityHandler;
+
 import java.util.*;
 
 public class InfeasibilityHandlerImpl implements InfeasibilityHandler {
 
     @Override
     public boolean isFeasible(Chromosome individual) {
-        List<?> genes = individual.getGenes();
-        Set<?> unique = new HashSet<>(genes);
-        return unique.size() == genes.size();
+        if (individual instanceof SoftComputing.Chromosomes.IntegerChromosome) {
+            SoftComputing.Chromosomes.IntegerChromosome ind =
+                    (SoftComputing.Chromosomes.IntegerChromosome) individual;
+            List<Integer> genes = ind.getGenes();
+            Set<Integer> unique = new HashSet<>(genes);
+            return unique.size() == genes.size();
+        }
+        return true;
     }
 
     @Override
     public Chromosome repair(Chromosome individual) {
-        List<?> genes = new ArrayList<>(individual.getGenes());
-        Set<Object> seen = new HashSet<>();
-        Random rand = new Random();
+        if (individual instanceof SoftComputing.Chromosomes.IntegerChromosome) {
+            SoftComputing.Chromosomes.IntegerChromosome ind =
+                    (SoftComputing.Chromosomes.IntegerChromosome) individual;
+            List<Integer> genes = new ArrayList<>(ind.getGenes());
+            Set<Integer> seen = new HashSet<>();
+            Random rand = new Random();
 
-        for (int i = 0; i < genes.size(); i++) {
-            Object gene = genes.get(i);
-            if (seen.contains(gene)) {
-                Object newGene;
-                do {
-                    newGene = rand.nextInt(genes.size());
-                } while (seen.contains(newGene));
-                ((List<Object>) genes).set(i, newGene);
+            for (int i = 0; i < genes.size(); i++) {
+                int gene = genes.get(i);
+                if (seen.contains(gene)) {
+                    int newGene;
+                    do {
+                        newGene = rand.nextInt(genes.size());
+                    } while (seen.contains(newGene));
+                    genes.set(i, newGene);
+                }
+                seen.add(genes.get(i));
             }
-            seen.add(genes.get(i));
-        }
 
-        Chromosome copy = individual.copy();
-        for (int i = 0; i < genes.size(); i++) {
-            copy.setGene(i, (double) genes.get(i));
+            SoftComputing.Chromosomes.IntegerChromosome fixed =
+                    new SoftComputing.Chromosomes.IntegerChromosome(genes.size(), 0, genes.size() - 1);
+            for (int i = 0; i < genes.size(); i++) {
+                fixed.setGene(i, genes.get(i));
+            }
+            return fixed;
         }
-
-        return copy;
+        return individual.copy();
     }
 }
--- Genetic_Algorithm_PH1/src/main/java/SoftComputing/interfaces/Replacement.java
+++ Genetic_Algorithm_PH1/src/main/java/SoftComputing/interfaces/Replacement.java
@@ -1,4 +1,8 @@
 package SoftComputing.interfaces;
 
+import SoftComputing.Chromosomes.Chromosome;
+import java.util.List;
+
 public interface Replacement {
+    List<Chromosome> replace(List<Chromosome> oldPopulation, List<Chromosome> newPopulation);
 }
